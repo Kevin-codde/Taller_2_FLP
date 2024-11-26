5636b39e39bc2d3d430320b05fe82416d902f1ad
unknown <bejaranok537@gmail.com>
2024-11-25 21:40:59 -1200

    Autores

--- 5.InterpretadorAsignacion.rkt
+++ 5.InterpretadorAsignacion.rkt
@@ -1,4 +1,13 @@
 #lang eopl
+#|
+
+   Autores: Kevin Andres Bejarano - 2067678
+            Juan David Gutierrez Florez - 2060104
+            Johan Sebastián Laverde pineda - 2266278
+            Johan Sebastian Acosta Restrepo 2380393
+   
+|#
+
 (define especificacion-lexica
   '(
     (espacio-blanco (whitespace) skip)
